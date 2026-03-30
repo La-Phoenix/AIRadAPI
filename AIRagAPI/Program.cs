c740ae10acd9ce78bd29c28c56e1589d0cc38a6e
la-phoenix <samuelokundalaiye@gmail.com>
2026-03-30 14:22:21 +0100

    Modified program.cs to fix oauth issue

--- AIRagAPI/Program.cs
+++ AIRagAPI/Program.cs
@@ -165,6 +165,21 @@ app.UseSwagger();
 app.UseSwaggerUI();
 app.UseExceptionHandler();
 
+if (app.Environment.IsProduction())
+{
+    app.Use(async (context, next) =>
+    {
+        context.Response.Headers["X-Content-Type-Options"] = "nosniff";
+        context.Response.Headers["X-Frame-Options"] = "DENY";
+        context.Response.Headers["X-XSS-Protection"] = "1; mode=block";
+        context.Response.Headers["Referrer-Policy"] = "strict-origin-when-cross-origin";
+        context.Response.Headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains";
+        await next();
+    });
+
+    app.UseHttpsRedirection();
+}
+
 // if (!app.Environment.IsEnvironment("Docker"))
 // {
 //     app.UseHttpsRedirection();
